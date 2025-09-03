1f10e9d0300ab83d203de178e07d16a858e8b81a
Malak <malakhussienosman@gmail.com>
2025-09-04 02:23:28 +0300

    Add Auth Login/Signup

---
+++ SocialMedia.BLL/ModelVM/User/UserVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.BLL.ModelVM.User
+{
+    public class UserVM
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string? Headline { get; set; }
+        public string? Bio { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Location { get; set; }
+
+        public List<string> PostBody { get; set; }
+        public List<string> Skills { get; set; }
+        public int Age { get; set; }
+        public int ConnectionsCount { get; set; }
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
+    }
+}
---
+++ SocialMedia.DAL/Entity/User.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+
+namespace SocialMedia.DAL.Entity
+{
+    public class User:IdentityUser
+    {
+        public User()
+        {
+
+        }
+        public User(string? name, string?bio, string? createdBy, string? imagePath)
+        {
+            Name = name;
+            Bio = bio;
+            CreatedBy = createdBy;
+            CreatedOn = DateTime.Now;
+            ImagePath = imagePath;
+
+          
+        }
+
+        
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public string? Bio { get; set; }           
+        public string? Headline { get; set; }   
+        public string? Location { get; set; }
+        public string? ImagePath { get; set; }
+
+
+        public string? CreatedBy { get; private set; }//Identity
+        public DateTime? CreatedOn { get; private set; }
+        public bool? IsDeleted { get; private set; }
+        public DateTime? DeletedOn { get; private set; }
+        public string? ModifiedBy { get; private set; }//Identity
+        public DateTime? ModifiedOn { get; private set; }
+
+
+        public int ConnectionsCount { get; set; }
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
+
+
+         public virtual List<Posts> Post { get; private set; }
+        public bool Update(string modifierUser, string name, int age)
+        {
+            if (string.IsNullOrEmpty(modifierUser))
+                return false;
+            Name = name;
+            Age = age;
+            ModifiedOn = DateTime.Now;
+            ModifiedBy = modifierUser;
+            return true;
+        }
+
+        internal bool ToggelStatus(string v)
+        {
+            throw new NotImplementedException();
+        }
+        //public bool ToggelStatus(string DeletedUser)
+        //{
+
+        //    if (string.IsNullOrEmpty(DeletedUser))
+        //        return false;
+        //    IsDeleted = !IsDeleted;
+        //    DeletedOn = DateTime.Now;
+        //    return true;
+        //}
+    }
+}
