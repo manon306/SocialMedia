cf7b39ec61905969edabae0d03b6eff42901f0c0
Menna hesham <mh8391802@gmail.com>
2025-09-04 18:18:52 +0300

    update

---
+++ SocialMedia.BLL/ModelVM/Comment/UpdateCommentVm.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialMedia.BLL.ModelVM.Comment
+{
+    public class UpdateCommentVm
+    {
+        public int ID { get;  set; }
+        public string Content { get; set; }
+        public int PostID { get; set; }
+        public string? UpdatedBy { get;  set; }
+    }
+}
