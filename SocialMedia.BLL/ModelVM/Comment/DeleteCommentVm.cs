90dbb83559c05795ca1422746ccd881f5f272cb3
Menna hesham <mh8391802@gmail.com>
2025-09-04 18:18:52 +0300

    update

---
+++ SocialMedia.BLL/ModelVM/Comment/DeleteCommentVm.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialMedia.BLL.ModelVM.Comment
+{
+    public class DeleteCommentVm
+    {
+        public int Id { get; set; }
+        public string? DeletedBy { get;  set; }
+    }
+}
---
+++ SocialMedia.BLL/Service/Implementation/CommentService.cs
@@ -0,0 +1,85 @@
+﻿using SocialMedia.BLL.ModelVM.Comment;
+using SocialMedia.DAL.Entity;
+
+namespace SocialMedia.BLL.Service.Implementation
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepo repo;
+        private readonly IMapper mapper;
+        public CommentService(ICommentRepo repo, IMapper mapper)
+        {
+            this.repo = repo;
+            this.mapper = mapper;
+        }
+
+        public (bool, string) AddComment(AddCommentVm comment)
+        {
+            //validation
+            if (comment == null)
+            {
+                return (false, "Cannot be null");
+            }
+            //Mapping
+            var commentEntity = new Comment(comment.Content, comment.PostID, comment.CreatedBy);
+            if (commentEntity == null)
+            {
+                return (false, "faild to create new Object");
+            }
+            //use repo
+            var result = repo.AddComment(commentEntity);
+            if(result.Item1 == false)
+            {
+                return (false, result.Item2);
+            }
+            return (true ,null);
+        }
+        public (bool, string) DeleteComment(DeleteCommentVm comment)
+        {
+            //validation
+            if (comment == null)
+            {
+                return (false, "Cannot delete empty Comment");
+            }
+            // use repo
+            var result = repo.DeleteComment(comment.Id, comment.DeletedBy);
+            if (result.Item1 == false)
+            {
+                return (false, result.Item2);
+            }
+            return (true ,null);
+        }
+        public (bool , string) UpdateComment(UpdateCommentVm comment)
+        {
+            //validation
+            if (comment == null)
+            {
+                return (false, "Cannot be null");
+            }
+            var entity = mapper.Map<Comment>(comment);
+            //repo
+            var result = repo.UpdateComment(entity);
+            if (result.Item1 == false)
+            {
+                return (false, result.Item2);
+            }
+            return (true ,null);
+        }
+        public (bool , string ,List<GetCommentVm>) GetAllComment(int postId)
+        {
+            //validation
+            if(postId <= 0)
+            {
+                return (false, "No Post Found", null);
+            }
+            //repo
+            var result = repo.GetAllComments(postId);
+            var entity = mapper.Map<List<GetCommentVm>>(result.Item3);
+            if (result.Item1 == false)
+            {
+                return (false, result.Item2, null);
+            }
+            return (true, null, entity);
+        }
+    }
+}
