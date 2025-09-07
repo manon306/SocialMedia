df5a4f22a6eef5caa7921b33404fbc8e17974bce
Malak <malakhussienosman@gmail.com>
2025-09-08 02:46:51 +0300

    Add SignOut

--- SocialMedia.BLL/Common/global.cs
+++ SocialMedia.BLL/Common/global.cs
@@ -5,7 +5,6 @@ global using AutoMapper;
 global using SocialMedia.BLL.ModelVM.Post;
 global using Microsoft.AspNetCore.Http;
 global using System.ComponentModel.DataAnnotations;
-global using Hangfire;
 global using SocialMedia.BLL.Helper;
 
 
